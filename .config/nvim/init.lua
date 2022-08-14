6c5b908b0ae167f4b697878f601341cb32f3e92f
Alexandre Louvet <alexandre.louvet8@gmail.com>
2022-08-14 16:50:58 +0200

    update LSP

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -91,9 +91,40 @@ require("mason").setup({
 	},
 })
 require("mason-lspconfig").setup({
-	ensure_installed = { "html", "hls", "tsserver", "texlab", "sumneko_lua", "marksman", "pyright", "rust_analyzer" },
+	ensure_installed = {
+		"html",
+		"hls",
+		"tsserver",
+		"texlab",
+		"sumneko_lua",
+		"marksman",
+		"pyright",
+		"rust_analyzer",
+		"volar",
+	},
 	automatic_installation = true,
 })
+
+-- LSP config
+require("lspconfig").html.setup({})
+require("lspconfig").hls.setup({})
+require("lspconfig").tsserver.setup({})
+require("lspconfig").texlab.setup({})
+require("lspconfig").sumneko_lua.setup({
+	on_attach = function(client, bufnr)
+		client.resolved_capabilities.document_formatting = false
+		vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>fm", "<cmd>lua vim.lsp.buf.formatting()<CR>", {})
+	end,
+})
+require("lspconfig").marksman.setup({})
+require("lspconfig").pyright.setup({})
+require("lspconfig").rust_analyzer.setup({
+	on_attach = function(client, bufnr)
+		client.resolved_capabilities.document_formatting = false
+		vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>fm", "<cmd>lua vim.lsp.buf.formatting()<CR>", {})
+	end,
+})
+require("lspconfig").volar.setup({})
 
 require("telescope").setup({
 	defaults = {
@@ -155,25 +186,5 @@ null_ls.setup({
 				end,
 			})
 		end
-	end,
-})
-
--- LSP config
-require("lspconfig").html.setup({})
-require("lspconfig").hls.setup({})
-require("lspconfig").tsserver.setup({})
-require("lspconfig").texlab.setup({})
-require("lspconfig").sumneko_lua.setup({
-	on_attach = function(client, bufnr)
-		client.resolved_capabilities.document_formatting = false
-		vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>fm", "<cmd>lua vim.lsp.buf.formatting()<CR>", {})
-	end,
-})
-require("lspconfig").marksman.setup({})
-require("lspconfig").pyright.setup({})
-require("lspconfig").rust_analyzer.setup({
-	on_attach = function(client, bufnr)
-		client.resolved_capabilities.document_formatting = false
-		vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>fm", "<cmd>lua vim.lsp.buf.formatting()<CR>", {})
 	end,
 })
