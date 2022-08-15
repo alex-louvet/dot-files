f37e84c221bfee4db70ae642edc3cafa3bed9d79
Alexandre Louvet <alexandre.louvet8@gmail.com>
2022-08-15 20:16:54 +0200

    some more tweaks

--- .config/fish/functions/update.fish
+++ .config/fish/functions/update.fish
@@ -1,4 +1,5 @@
 function update --wraps='paru && sudo paru -Rs (paru -Qqtd)' --description 'alias update paru && sudo paru -Rs (paru -Qqtd)'
-  paru -Syu $argv && sudo paru -Rs (paru -Qqtd); 
-  xmonad --recompile
+    paru -Syu $argv && sudo paru -Rs (paru -Qqtd); 
+    xmonad --recompile
+    bash ~/.config/polybar/launch.sh
 end
--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,7 +4,7 @@ if type "xrandr" ; then
     index=0
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
         barname="mainbar$index"
-        monitor=$m polybar --reload $barname &
+        monitor=$m polybar --reload $barname -q &
         let index=index+1
     done
 else
