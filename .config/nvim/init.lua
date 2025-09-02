fab582ea78dfa3e6ed2a6fb4eac1fd7ff555dc37
Alexandre Louvet <alexandre.louvet@mailo.fr>
2025-09-02 05:47:11 +0200

    python-lsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -622,7 +622,29 @@ require('lazy').setup({
       local servers = {
         clangd = {},
         -- gopls = {},
-        pyright = {},
+        -- pyright = {},
+        pylsp = {
+          settings = {
+            pylsp = {
+              plugins = {
+                -- formatter options
+                black = { enabled = false },
+                autopep8 = { enabled = false },
+                yapf = { enabled = false },
+                pyls_isort = { enabled = false },
+                -- linter options
+                pylint = { enabled = true, executable = 'pylint' },
+                pyflakes = { enabled = false },
+                pycodestyle = { enabled = false },
+                -- type checker
+                pylsp_mypy = { enabled = true },
+                -- auto-completion options
+                jedi_completion = { fuzzy = false },
+                -- rope = { enable = false },
+              },
+            },
+          },
+        },
         rust_analyzer = {},
         tinymist = {
           root_dir = function(filename, bufnr)
