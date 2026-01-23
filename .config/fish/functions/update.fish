3f138c3f2c3db4292b350854a42647e6579b004e
Alexandre Louvet <alexandre.louvet@inria.fr>
2026-01-23 10:20:37 +0100

    zen updates

--- .config/fish/functions/update.fish
+++ .config/fish/functions/update.fish
@@ -1,4 +1,4 @@
 function update --wraps='sudo apt update && sudo apt upgrade && snap refresh && sudo bash /home/allouvet/Downloads/zen.sh --install' --description 'alias update=sudo apt update && sudo apt upgrade && sudo snap refresh && sudo bash /home/allouvet/Downloads/zen.sh --install'
-  sudo apt update && sudo apt upgrade && sudo snap refresh && uv tool upgrade --all $argv
+  sudo apt update && sudo apt upgrade && sudo snap refresh && uv tool upgrade --all && bash /home/allouvet/bin/zen.sh $argv
         
 end
